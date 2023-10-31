542db6bea53373a4ddea349336a33bc96587b7d5
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-10-31 14:16:39 +0300

    add ts alias

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const App: FC = () => {
+  return (
+    <>
+      <p> App</p>
+      <p> App</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
+import App from "@app/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <>
-      <p>fdgsdfgsdgsdf</p>
-      <p>fdgsdfgsdgsdf</p>
-      <p>fdgsdfgsdgsdf</p>
+      <App />
     </>
   </React.StrictMode>
 );
