31e466348a6ecf335d586b6756edb7a14f40079c
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-10-31 13:35:59 +0300

    start config

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <>
+      <p>fdgsdfgsdgsdf</p>
+      <p>fdgsdfgsdgsdf</p>
+      <p>fdgsdfgsdgsdf</p>
+    </>
+  </React.StrictMode>
+);
