6fd30e977485fea84c72f1284999bce3f3234785
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-11-17 14:01:45 +0300

    add post css from mantine

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,8 @@ import { ErrorBoundary } from "./providers/ErrorBoundary";
 import { MantineProvider } from "./providers/MantineProvider";
 import { AppRouter } from "./router";
 
+import "@app/styles/globals.css";
+
 const App: FC = () => {
   return (
     <MantineProvider>
--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 
+import styles from "./Layout.module.css";
+
 export const Layout: FC = () => {
   return (
-    <div>
-      <div>left menu</div>
-      <div>
-        <div>header</div>
+    <>
+      <div className={styles.menuWrapp}>left</div>
+      <div className={styles.mainWrapp}>
+        <div className={styles.headerWrapp}>header</div>
         <Outlet />
         <div>footer</div>
       </div>
-    </div>
+    </>
   );
 };
