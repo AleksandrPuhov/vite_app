e39657ae21c3164623d6382e1c24346d4f8763f1
Aleksandr Puhov <aleksandrpuhov123@gmail.com>
2023-11-22 14:03:38 +0300

    add socials icons in footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,11 +1,48 @@
-import { FC } from "react";
+import { FC, useCallback, useMemo } from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./Footer.module.css";
 
-import { ActionIcon, Container, Group, rem } from "@mantine/core";
 import { VKIcon } from "@shared/ui/icons/socials/vk";
 
+import { ActionIcon, Container, Group, rem, Text } from "@mantine/core";
+
+const FOOTER_SOCIALS_ICONS = [
+  {
+    variant: "vk",
+    to: "https://vk.com/",
+  },
+];
+
 export const Footer: FC = () => {
+  const icon = useCallback(
+    (
+      link: { variant: string; to: string },
+      style?: React.CSSProperties | undefined
+    ) => {
+      switch (link.variant) {
+        case "vk": {
+          return <VKIcon style={style} />;
+        }
+        default:
+          return null;
+      }
+    },
+    []
+  );
+
+  const socialsIcons = useMemo(() => {
+    return FOOTER_SOCIALS_ICONS.map((link) => {
+      return (
+        <Link to={link.to} target="_blank" key={link.to}>
+          <ActionIcon size="lg" variant="subtle" radius="xl">
+            {icon(link, { width: rem(18), height: rem(18) })}
+          </ActionIcon>
+        </Link>
+      );
+    });
+  }, [icon]);
+
   return (
     <footer className={styles.root}>
       <Container className={styles.inner} fluid>
@@ -19,14 +56,11 @@ export const Footer: FC = () => {
         {/* <div className={classes.groups}>{groups}</div> */}
       </Container>
       <Container className={styles.afterFooter} fluid>
-        <div>left</div>
-        {/* <Text c="dimmed" size="sm">
-          © 2020 mantine.dev. All rights reserved.
-        </Text> */}
+        <Text c="dimmed" size="sm">
+          © 2023 Mantine project.
+        </Text>
         <Group gap={0} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <VKIcon style={{ width: rem(18), height: rem(18) }} />
-          </ActionIcon>
+          {socialsIcons}
         </Group>
       </Container>
     </footer>
